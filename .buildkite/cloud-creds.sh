25ad3b98ac89676c5e82c1a6f7f9158cc4df4c9a
dantedelucia <dante.delucia@gmail.com>
2025-07-22 14:36:38 -0700

    Fetch google service-account-key

--- .buildkite/cloud-creds.sh
+++ .buildkite/cloud-creds.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-
 echo "INFO: Setting up environment for cloud: ${CLOUD:-unspecified}"
 
 # Always required
@@ -18,7 +17,17 @@ gcp)
 	: "${GOOGLE_PROJECT:=svmkit}"
 	: "${GOOGLE_REGION:=us-central1}"
 	: "${GOOGLE_ZONE:=${GOOGLE_REGION}-a}"
-
+	GCP_SA_KEY="$(buildkite-agent secret get GCP_SA_KEY)"
+	mkdir -p /secrets
+	echo "$GCP_SA_KEY" >/secrets/sa-key.json
+	chmod 600 /secrets/sa-key.json
+	GOOGLE_APPLICATION_CREDENTIALS=/secrets/sa-key.json
+	gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+	gcloud config set project "$GOOGLE_PROJECT"
+	export GOOGLE_PROJECT \
+		GOOGLE_REGION \
+		GOOGLE_ZONE \
+		GOOGLE_APPLICATION_CREDENTIALS
 	;;
 
 *)
